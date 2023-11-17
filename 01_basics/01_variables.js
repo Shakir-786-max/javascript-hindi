676b6f181f45b2c06c99ee97fd4b4f6f70842a96
Shakir-786-max <115889585+Shakir-786-max@users.noreply.github.com>
2023-11-17 07:17:27 +0000

    yes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const AccountId = 12345
+let AccountEmail = "shakiralam4205@gmail.com"
+var AccountPassword = 258
+
+// AccountId = 369   // not allowed
+
+AccountEmail = "shakirpatelcivil@gmail.com"
+console.log(AccountEmail);
