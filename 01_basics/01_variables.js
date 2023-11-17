9b5f0b934115d386a392b49159f1b3c40cb30e21
Shakir-786-max <115889585+Shakir-786-max@users.noreply.github.com>
2023-11-17 09:36:57 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,16 @@
 const AccountId = 12345
 let AccountEmail = "shakiralam4205@gmail.com"
-var AccountPassword = 258
+var AccountPassword = "258" //prefer not to be used bcoz block issues.
+AccountCity = "jaipur"
+
+let accountState;
 
 // AccountId = 369   // not allowed
 
 AccountEmail = "shakirpatelcivil@gmail.com"
-console.log(AccountEmail);
+AccountPassword = "147"
+AccountCity = "patna"
+
+// console.log( AccountPassword, AccountCity);
+
+console.table([AccountEmail, AccountId, AccountPassword, AccountCity, accountState]);
