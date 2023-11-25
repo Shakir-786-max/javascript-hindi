29ff12e4fc0483ede227a7336cc63f5aec8e3513
Shakir-786-max <115889585+Shakir-786-max@users.noreply.github.com>
2023-11-25 16:31:54 +0000

    fgfty

---
+++ 01_basics/03_conversion-opration.js
@@ -0,0 +1,41 @@
+// let score = 33 // case 1
+// console.log(typeof score);
+
+//case 2//
+// let score = undefined
+// console.log(typeof score);
+
+//changeing datatypes number//
+//  let valueinNumber = Number(score)
+//  console.log(typeof valueinNumber);
+//  console.log(valueinNumber);//NAN=> not a number.
+
+//  //changing datatypes in string//
+//  let valueinString = String(score)
+//  console.log(valueinString);
+//  console.log(typeof valueinString);
+
+
+// 33=>33
+//33abc=>NAN
+//true=>1
+//false=>0
+
+let city="atna"
+// let booleancity=Boolean(city)
+console.log(city);
+
+// 1=>True
+//False=> false
+//""=> false
+//"abc"=>True
+
+let Number = 12
+let stringNumber= String(Number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let loggedIn = true
+let booleanloggedIn = Boolean(loggedIn)
+console.log(loggedIn);  
+console.log(typeof booleanloggedIn);
