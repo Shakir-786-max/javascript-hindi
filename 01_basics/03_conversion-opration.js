3a699c414edc01eaaf326f222e68c6b354287580
Shakir-786-max <115889585+Shakir-786-max@users.noreply.github.com>
2023-11-25 18:21:18 +0000

    oprations

--- 01_basics/03_conversion-opration.js
+++ 01_basics/03_conversion-opration.js
@@ -39,3 +39,36 @@ let loggedIn = true
 let booleanloggedIn = Boolean(loggedIn)
 console.log(loggedIn);  
 console.log(typeof booleanloggedIn);
+
+
+
+///////////////////////////oprations///////////////////
+
+//   for negative value
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// for calculations
+console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// For adding string
+let str1 = "hello"
+let str2 =" shakir"
+let str3 = str1 + str2
+console.log(str3);
+
+
+//behavior of string=> if string is in first, after  all the value will be treated as is it.only before string value will be calculated.
+console.log("2" + 2 + 3); // output will be 223.
+console.log(2 + 2 + "3"); // output will be 43
+
+///prefix and postfix
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);// output will be 101.
